3421b5d3f75f5533a5fc50e1838f37853cf7ad7a
lovecloudzzz <lovecloudez@gmail.com>
2022-11-12 20:09:15 +0300

    update

--- app.py
+++ app.py
@@ -1,20 +1,85 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for, session
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from db_utility import DataBase
+from db_utility import BaseDB, UserDB, ProductDB, FavoritesDB, CartDB, OrdersDB, OrdersProductsDB
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1zZUwWyKXeZln_OKHhiRkw'
+login_manager = LoginManager(app)
+login_manager.login_view = 'register'
 
-db = DataBase()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserDB().get_user_by_id(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('/'))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     context = {
-        'title': 'SakuraStore'
+        'title': 'SakuraStore',
+        'products': ProductDB.all_products()
     }
-    return render_template("main.html", **context)
+    return render_template('main.html', **context)
 
+
+@login_required
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    context = {
+        'title': 'Профиль'
+    }
+    return render_template("profile.html", **context)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    context = {
+        'title': 'Вход'
+    }
+    return render_template("login.html", **context)
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    context = {
+        'title': 'Регистрация'
+    }
+    return render_template("registration.html", **context)
+
+
+
+@login_required
+@app.route('/orders', methods=['GET', 'POST'])
+def orders():
+    context = {
+        'title': 'Заказы'
+    }
+    return render_template("orders.html", **context)
+
+
+@login_required
+@app.route('/cart', methods=['GET', 'POST'])
+def cart():
+    context = {
+        'title': 'Корзина'
+    }
+    return render_template("cart.html", **context)
+
+
+@login_required
+@app.route('/favorites', methods=['GET', 'POST'])
+def favorites():
+    context = {
+        'title': 'Избранное'
+    }
+    return render_template("favorites.html", **context)
 
 if __name__ == '__main__':
     app.run()
--- db_utility.py
+++ db_utility.py
@@ -1,30 +1,134 @@
 import psycopg2
 
 
-class DataBase:
-    def __init__(self):
-        self.con = psycopg2.connect(
-            dbname="",
-            user="",
-            password="",
-            host="localhost",
-            port=5432
-        )
-        self.con.autocommit = True
-        self.cur = self.con.cursor()
+class BaseDB:
+    con = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="",
+        host="localhost",
+        port=5432
+    )
+    con.autocommit = True
+    cur = con.cursor()
 
-    def get_user_by_email(self, email):
-        self.cur.execute("SELECT * from users WHERE email = '{email}'")
-        return self.cur.fetchone()
 
-    def get_user_by_id(self, id):
-        self.cur.execute("SELECT * from users WHERE id = '{id}'")
-        return self.cur.fetchone()
+class UserDB(BaseDB):
 
-    def all_products(self):
-        self.cur.execute("SELECT * from products")
-        return self.cur.fetchall()
+    @classmethod
+    def register_user(cls, login, email, password):
+        UserDB.cur.execute(
+            "INSERT INTO users (login, email, password) VALUES (%s, %s, %s)",
+            (login, email, password))
 
-    def get_product_by_id(self, id):
-        self.cur.execute("SELECT * from products WHERE id = '{id}'")
-        return self.cur.fetchone()
+    @classmethod
+    def get_user_by_id(cls, id):
+        UserDB.cur.execute("SELECT * from users WHERE id = '{id}'")
+        return UserDB.cur.fetchone()
+
+    @classmethod
+    def get_user_by_email(cls, email):
+        UserDB.cur.execute("SELECT * from users WHERE email = '{email}'")
+        return UserDB.cur.fetchone()
+
+
+    def fromDB(self, user_id, db):
+        self.__user = UserDB.get_user_by_id(user_id)
+        return self
+
+    def create(self, user):
+        self.__user = user
+        return self
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    @property
+    def get_id(self):
+        return str(self.__user[0])
+
+    @property
+    def is_admin(self):
+        return self.__user[4]
+
+class ProductDB(BaseDB):
+    @classmethod
+    def all_products(cls):
+        ProductDB.cur.execute("SELECT * from products")
+        return ProductDB.cur.fetchall()
+
+    @classmethod
+    def get_product_by_id(cls, id):
+        ProductDB.cur.execute("SELECT * from products WHERE id = '{id}'")
+        return ProductDB.cur.fetchone()
+
+    @classmethod
+    def delete_product_by_id(cls, id):
+        ProductDB.cur.execute("DELETE FROM products WHERE id = '%s'" % id)
+
+
+class OrdersDB(BaseDB):
+    @classmethod
+    def all_orders(cls):
+        OrdersDB.cur.execute("SELECT * from products")
+        return OrdersDB.cur.fetchall()
+
+    @classmethod
+    def get_order_by_id(cls, id):
+        OrdersDB.cur.execute("SELECT * from products WHERE id = '{id}'")
+        return OrdersDB.cur.fetchone()
+
+
+class FavoritesDB(BaseDB):
+    @classmethod
+    def all_favorites(cls):
+        FavoritesDB.cur.execute("SELECT * from favorites")
+        return FavoritesDB.cur.fetchall()
+
+    @classmethod
+    def add_favorite(cls, user_id, product_id):
+        FavoritesDB.cur.execute("INSERT INTO favorites (user_id, product_id) VALUES (%s, %s)" % (user_id, product_id))
+
+    @classmethod
+    def remove_favorite(cls, user_id, product_id):
+        FavoritesDB.cur.execute("DELETE FROM favorites WHERE user_id = %s and product_id = %s" % (user_id, product_id))
+
+    @classmethod
+    def get_all_favourites(cls, user_id):
+        FavoritesDB.cur.execute("SELECT id, title, annotation FROM products JOIN favorites f "
+                         "ON products.id = f.product_id "
+                         "WHERE user_id = %s" % user_id)
+        return FavoritesDB.cur.fetchall()
+
+
+class CartDB(BaseDB):
+    @classmethod
+    def all_favorites(cls):
+        CartDB.cur.execute("SELECT * from carts")
+        return CartDB.cur.fetchall()
+
+    @classmethod
+    def get_favorite_by_id(cls, id):
+        CartDB.cur.execute("SELECT * from carts WHERE id = '{id}'")
+        return CartDB.cur.fetchone()
+
+
+class OrdersProductsDB(BaseDB):
+    @classmethod
+    def all_favorites(cls):
+        OrdersProductsDB.cur.execute("SELECT * from orders_products")
+        return OrdersProductsDB.cur.fetchall()
+
+    @classmethod
+    def get_favorite_by_id(cls, id):
+        OrdersProductsDB.cur.execute("SELECT * from orders_products WHERE id = '{id}'")
+        return OrdersProductsDB.cur.fetchone()
