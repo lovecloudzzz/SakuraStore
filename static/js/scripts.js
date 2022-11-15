9e81603be32e89b19af787e96a9b78d4ae8fdbd9
lovecloudzzz <lovecloudez@gmail.com>
2022-11-16 00:10:07 +0300

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,7 +3,7 @@ function checkRegForm(event) {
     let email = document.getElementById("email")
     let password = document.getElementById("password")
     let confirm_password = document.getElementById("confirm_password")
-    if ((login.value.replaceAll(" ", "") === '') || (email.value.replaceAll(" ", "") === '') || (password.value.replaceAll(" ", "") === '') || (confirm_password.value.replaceAll(" ", "") === ''))
+    if ((login.value.replaceAll(" ", "") === '') || (email.value.replaceAll(" ", "") === '') || (password.value.replaceAll(" ", "") === '') || (confirm_password.value.replaceAll(" ", "") === '') || (password.value.includes(" ")) || (email.value.includes(" ")) || (login.value.includes(" ")) || (confirm_password.value.includes(" ")))
     {
         window.alert('Пустые поля')
         event.preventDefault()
@@ -18,7 +18,7 @@ function checkRegForm(event) {
 function checkLogForm(event) {
     let email = document.getElementById("email")
     let password = document.getElementById("password")
-    if ((email.value.replaceAll(" ", "") === '') || (password.value.replaceAll(" ", "") === ''))
+    if ((email.value.replaceAll(" ", "") === '') || (password.value.replaceAll(" ", "") === '') || password.value.find(" ") || (password.value.includes(" ")) || (email.value.includes(" ")))
     {
         window.alert('Пустые поля')
         event.preventDefault()
@@ -29,7 +29,7 @@ function checkLogForm(event) {
 function checkProfForm(event) {
     let login = document.getElementById("login")
     let password = document.getElementById("password")
-    if ((login.value.replaceAll(" ", "") === '') || (password.value.replaceAll(" ", "") === ''))
+    if ((login.value.replaceAll(" ", "") === '') || (password.value.replaceAll(" ", "") === '') || (password.value.includes(" ")) || (login.value.includes(" ")))
     {
         window.alert('Пустые поля')
         event.preventDefault()
